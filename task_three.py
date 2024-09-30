b3a6be5f3b05978490b54f5ffaf071a93ac1947e
prostomishutka <patserkovskyi1999@gmail.com>
2024-09-30 21:53:25 +0300

    Mistakes are solved

--- task_three.py
+++ task_three.py
@@ -1,30 +1,13 @@
 import re
 
 def normalize_phone(phone_number):
-    list_numbers = []
-    for num in phone_number:
-        pat_one = r"[^\d+]"
-        delet_symb = re.sub(pat_one, "", num)
-        
-        if  not re.match(r"^\+380", delet_symb):
-            if not re.match(r"^380", delet_symb):
-                delet_symb = "+38" + delet_symb
-            else:
-                delet_symb = "+" + delet_symb
-            list_numbers.append(delet_symb)
-        else:
-            list_numbers.append(delet_symb)
-    return f"Нормалізовані номери телефонів для SMS-розсилки: {list_numbers}"
-
-raw_numbers = [
-    "067\\t123 4567",
-    "(095) 234-5678\\n",
-    "+380 44 123 4567",
-    "380501234567",
-    "    +38(050)123-32-34",
-    "     0503451234",
-    "(050)8889900",
-    "38050-111-22-22",
-    "38050 111 22 11   ",
-]
-print(normalize_phone(raw_numbers))
+    pattern = r"[^\d+]"
+    number = re.sub(pattern, "", phone_number)
+    if "+38" not in number:
+        return "+38" + number
+    elif "+" not in number:
+        return "+" + number
+    else:
+        return number
+    
+print(normalize_phone("067\\t123 4567"))
--- task_two.py
+++ task_two.py
@@ -1,27 +1,19 @@
 import random
 
 def get_numbers_ticket(min, max, quantity):
-    while True:
-        try:
-            collec_unic_num = set(range(min, max+1))
-            if min < 1 or max > 1000 or (min > quantity > max):
-                print("The value entered is incorrect, please enter the minimum" \
-                    "number, maximum number, and the quantity of numbers" \
-                    "in the format: min, max, quantity.")
-                min = int(input("Input min: "))
-                max = int(input("Input max: "))
-                quantity = int(input("Input quantity: "))
-            else:
-                random_nums = random.sample(list(collec_unic_num), quantity)
-                sorted_list_nums = sorted(random_nums)
-                return f"Ваші лотерейські числа: {sorted_list_nums}"
-        
-        except (TypeError, ValueError):
-            print("The value entered is incorrect, please enter the minimum" \
-                    "number, maximum number, and the quantity of numbers" \
-                    "in the format: min, max, quantity.")
-            min = int(input("Input min: "))
-            max = int(input("Input max: "))
-            quantity = int(input("Input quantity: "))
+    try:
+        list_numbers = []
+        random_numbers = []
+        if min >= 1 and max <= 1000 and min <= quantity <= max:
+            for num in range(min, max + 1):
+                list_numbers.append(num)
+            random_numbers = random.sample(list_numbers, quantity)
+            random_numbers.sort()
+            return random_numbers
+        else:
+            return list_numbers
+    except TypeError:
+        print("The data is incorrect.")
 
-print(get_numbers_ticket(1, 1000, 99))
+
+print(get_numbers_ticket(1, 49, 6))
