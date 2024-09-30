84568ee42c57db91fd52f15bfc84e7fc9afc94c5
prostomishutka <patserkovskyi1999@gmail.com>
2024-09-30 22:22:15 +0300

    New edition

--- task_three.py
+++ task_three.py
@@ -3,11 +3,11 @@ import re
 def normalize_phone(phone_number):
     pattern = r"[^\d+]"
     number = re.sub(pattern, "", phone_number)
-    if "+38" not in number:
-        return "+38" + number
-    elif "+" not in number:
+    if number.startswith("38"):
         return "+" + number
+    elif not number.startswith("+38"):
+        return "+38" + number
     else:
         return number
     
-print(normalize_phone("067\\t123 4567"))
+print(normalize_phone("38067\\t123 4567"))
