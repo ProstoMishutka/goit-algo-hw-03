2f8dd907179980f33e62adc284cda6c9c776a9fa
ProstoMishutka <96257420+ProstoMishutka@users.noreply.github.com>
2024-10-08 13:49:31 +0300

    Delete archivator.py

--- archivator.py
+++
@@ -1,7 +0,0 @@
-import shutil
-def archivator(name_arch: str, format_arch: str, root_dir_arch: str):
-    shutil.make_archive(base_name=name_arch,format=format_arch, root_dir=root_dir_arch)
-
-
-if __name__ == "__main__":
-    archivator(None)
